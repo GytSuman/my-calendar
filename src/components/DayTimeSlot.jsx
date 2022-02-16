cfac20dbfb8001c92a95780df9c55d703fa8d98c
GytSuman <sumanghosh@gytworkz.com>
2022-02-16 16:33:26 +0530

    day view

---
+++ src/components/DayTimeSlot.jsx
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material'
+import moment from 'moment'
+import React from 'react'
+import DayTime from './DayTime'
+import "./header.css"
+
+function DayTimeSlot({ time, days }) {
+  const formattedTime = moment().set ('hours', time).format ('h a')
+  return (
+    <>
+    <Grid container direction="row">
+      <Grid item xs={1} sx={{ paddingLeft: "16px", paddingTop: "8px"}}>{formattedTime}</Grid>
+      <Grid item xs={11} className='flex'>
+      {days && days.map(dayObj => (
+      <>
+      <DayTime key={dayObj.dateStamp} time={time} dateStamp={dayObj.dateStamp} dayName={dayObj.dayName}/>
+      </>
+    ))}
+      </Grid>
+    </Grid>
+    
+    </>
+  )
+}
+
+export default DayTimeSlot
--- src/dayUtils.js
+++ src/dayUtils.js
@@ -6,7 +6,8 @@ export const getDays = (currentDay = moment()) => {
  const day = Array.from(Array(1)).map((day, index) => index).map(day =>  moment(presentDay).add(day, "days").set("minutes", 0).set("seconds", 0)).map(momentObj => ({
   date: momentObj.date(),
   dateStamp: +momentObj,
-  dayName: momentObj.format("ddd")
+  dayName: momentObj.format("ddd"),
+  monthName: momentObj.format("MMMM")
  }))
   console.log(day);
 
